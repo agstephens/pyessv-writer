ae22e03da016889875321fa217989411877d29db
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-04-11 14:20:29 +0200

    logging updated

--- sh/write_wcrp_cmip6.sh
+++ sh/write_wcrp_cmip6.sh
@@ -6,13 +6,13 @@ source $PYESSV_WRITER_HOME/sh/utils.sh
 # Main entry point.
 main()
 {
-	log "writing WCRP cmip6 cv ..."
+	log "writing WCRP cmip6 vocabs ..."
 
 	declare source=$1
 
 	python $PYESSV_WRITER_HOME/sh/write_wcrp_cmip6.py --source=$source
 
-	log "WCRP cmip6 cv written to "$HOME/.esdoc/pyessv-archive
+	log "WCRP cmip6 vocabs written to "$HOME/.esdoc/pyessv-archive
 }
 
 # Invoke entry point.
